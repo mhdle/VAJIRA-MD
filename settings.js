5b16f819f9f09cb82e2186518f532fd2c6a4ab10
mhdle <Monarqueitachi10@outlook.com>
2024-07-08 11:13:00 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=WY9SSC5K#op0Fd1mmcs-jrSrRR_cohoV0CmXC5w5nKW2cNPPArSI' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=6ck1SRJK#vr3Eydo8PFXfoF0otOWknj8RCgGAnypyk0Ecqs63inw' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
